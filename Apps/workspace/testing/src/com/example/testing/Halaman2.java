f35b5240c6d8e2b7c14d5b237257e50382323c1d
ivanlukman <ivanlukman93@gmail.com>
2015-02-18 12:47:08 +0700

    asdasdasd

---
+++ Apps/workspace/testing/src/com/example/testing/Halaman2.java
@@ -0,0 +1,15 @@
+package com.example.testing;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Halaman2 extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.halaman2);
+        
+    }
+    
+}
